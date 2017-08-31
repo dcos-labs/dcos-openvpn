be8403dc6bb0399dff049b392942b5480e777b49
Richard Shaw <richard@aggress.net>
2017-08-31 16:56:29 +0100

    Removed secrets to keep this available to DC/OS OSS replaced with Env Vars Amnended Readme

--- local_universe_setup.sh
+++ local_universe_setup.sh
@@ -6,16 +6,20 @@
 # Docker daemon on each DC/OS node configured to work with insecure registry
 # https://docs.docker.com/registry/insecure/ or secure your registry
 
-
-function build {
-  cd /Users/richard/code/
+function rebuild_universe {
   dcos config set core.dcos_url https://192.168.33.11
   dcos auth login --username=admin --password=password
+  rm -rf /Users/richars/code/universe
+  cd /Users/richard/code
   git clone https://github.com/mesosphere/universe --branch=version-3.x
   cd universe
-  cp -R repo/packages/O/openvpn-admin/1 repo/packages/O/openvpn-admin/2
-  sed -i -e 's/mesosphere\/dcos-openvpn/aggress\/dcos-openvpn/g' repo/packages/O/openvpn-admin/2/resource.json
-  sed -i -e 's/0.0.0-0.1/0.0.0-0.2/g' repo/packages/O/openvpn-admin/2/package.json
+  #cp -R repo/packages/O/openvpn/1
+  #sed -i -e 's/mesosphere\/dcos-openvpn/aggress\/dcos-openvpn/g' repo/packages/O/openvpn/1/resource.json
+  #sed -i -e 's/0.0.0-0.1/0.0.0-0.2/g' repo/packages/O/openvpn/1/package.json
+} 
+
+function build {
+  cd /Users/richard/code/universe
   scripts/build.sh
   DOCKER_IMAGE="192.168.33.10:5000/universe-server" DOCKER_TAG="universe-server" docker/server/build.bash
   DOCKER_IMAGE="192.168.33.10:5000/universe-server" DOCKER_TAG="universe-server" docker/server/build.bash publish
@@ -24,14 +28,16 @@ function build {
 }
 
 function remove {
-  dcos package uninstall openvpn-admin
+  dcos package uninstall openvpn
   dcos marathon app remove /universe
   dcos package repo remove universe-server
-  rm -rf universe
+  docker image rm -f 192.168.33.10:5000/universe-server:universe-server
+  docker rmi -f $(docker images -aq)
 }
 
 case "$@" in
   remove) remove ;;
   build)  build  ;;
+  rebuild_universe)  rebuild_universe  ;;
   *)      echo "build or remove";  exit 1 ;;
 esac
