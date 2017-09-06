fdf03b280a46f15e1caa63c6e6254547bad1a00f
Richard Shaw <richard@aggress.net>
2017-09-06 10:30:20 +0100

    Change znode

--- bin/envs.sh
+++ bin/envs.sh
@@ -1,6 +1,6 @@
 export CA_PASS=${CA_PASS:="nopass"}
 export CA_CN=${CA_CN:="openvpn.dcos"}
-export ZKPATH=${ZKPATH:="/dcos-vpn"}
+export ZKPATH=${ZKPATH:="/openvpn"}
 export ZKCLI=${ZKCLI:="zk-shell"}
 export ZKURL=${ZKURL:="master.mesos:2181"}
 export CONFIG_LOCATION=${CONFIG_LOCATION:="/etc/openvpn"}
