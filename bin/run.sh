dea065c50ec932d916c12307b58cc6ecf7f3f0e3
Richard Shaw <richard@aggress.net>
2017-12-07 16:51:06 +0000

    Removed check_status function and updated Changelog

--- bin/run.sh
+++ bin/run.sh
@@ -4,24 +4,7 @@
 # Vars, checks and admin
 ##############################
 
-container_files=0
-zookeeper_path=0
 source /dcos/bin/envs.sh
-
-# Check to see if the container already has the openvpn files locally
-# And the second checks for an existing znode in Zookeeper which suggests
-# This is being re-run after a previous setup
-
-function check_status {
-  if [ -f $CONFIG_LOCATION/openvpn.conf ]; then
-    container_files=1
-  fi
-  if [[ -z $(run_command "ls $ZKPATH/openvpn.conf") ]]; then
-    zookeeper_path=1
-  fi
-  echo "container_files = " $container_files
-  echo "zookeeper_path  = " $zookeeper_path
-}
 
 # Workaround to pass add_auth on the one liner as zk-shell doens't provide this as a param
 
@@ -30,12 +13,20 @@ function run_command {
   return $?
 }
 
+function fix_scripts {
+  # Fix a bug in zk-shell copy that's pending a pull request
+  sed -i 's/return PathValue("".os.path.join(fph.readlines()))/return PathValue("".join(os.path.join(fph.readlines())))/g' /usr/lib/python2.7/site-packages/zk_shell-1.1.3-py2.7.egg/zk_shell/copy.py
+  
+  # Replace the shipped easyrsa with our easyrsa to remove the revoke confirmation
+  sed -i 's/easyrsa/\/dcos\/bin\/easyrsa/g' /usr/local/bin/ovpn_revokeclient
+}
+
 ##############################
 # File download and upload
 ##############################
 
 function download_files {
-  if [[ $(run_command "ifind /openvpn/upload_marker") = "" ]]; then
+  if [[ $(run_command "find /openvpn/ upload_marker") = "" ]]; then
     ZKPATH_STRIPPED=$(echo $ZKPATH | sed -e 's/^\///')
     for fname in $(run_command "find / $ZKPATH_STRIPPED"); do
       local sub_path=$(echo $fname | cut -d/ -f3-)
@@ -48,28 +39,24 @@ function download_files {
       fi
     
       local fs_path=$CONFIG_LOCATION/$sub_path
-      run_command "cp $fname file://$fs_path false true false true"
+      run_command "cp $fname file://$fs_path false true false false" > /dev/null 2>&1
       # Directories are copied as empty files, remove them so that the
       # subsequent copies actually work.
       [ -s $fs_path ] || rm $fs_path
     done
   else
-    echo "Upload marker found, leaving until next cron run"
+    echo "INFO: Upload marker found, leaving until next cron run"
   fi
 }
 
 function upload_files {
-  if [ $zookeeper_path = 0 ]; then
-  	run_command "create $ZKPATH '' false false true"
-  	run_command "set_acls /$ZKPATH username_password:$OVPN_USERNAME:$OVPN_PASSWORD:cdrwa"
-  fi
 
   # Adding a marker so we know when all the files have been uploaded 
   run_command "create $ZKPATH/upload_marker ''"
 
   for fname in $(find $CONFIG_LOCATION -not -type d); do
     local zk_location=$(echo $fname | sed 's|'$CONFIG_LOCATION'/|/|')
-    run_command "cp file://$fname $ZKPATH$zk_location false true false true"
+    run_command "cp file://$fname $ZKPATH$zk_location false true false true" > /dev/null 2>&1
   done
 
   # Removing upload marker 
@@ -87,18 +74,19 @@ function synchronise {
   index_tmp="/tmp/index.txt"
 
   rm -f $index_tmp
-  run_command "cp $index_on_zk file://$index_tmp false true false true"
+  run_command "cp $index_on_zk file://$index_tmp false true false true" > /dev/null 2>&1
   if [[ $(diff -q $index_on_local $index_tmp) != "" ]]; then
-    if [[ $(run_command "ifind /openvpn/upload_marker") = "" ]]; then
+    if [[ $(run_command "find /openvpn/ upload_marker") = "" ]]; then
+      echo "INFO: Zookeeper has a new dataset, downloading and restarting OpenVPN to apply"
       download_files
       pkill openvpn
       ovpn_run --daemon
       set_public_location
     else
-      echo "Upload marker found will attempt on next cron run"
+      echo "INFO: Upload marker found, will attempt on next cron run"
     fi
   else
-    echo "index.txt matches so nothing to do"
+    echo "INFO: No changes found on Zookeeper"
   fi
 }
 
@@ -113,6 +101,7 @@ function get_location {
 
 function set_public_location {
   source $OPENVPN/ovpn_env.sh
+  echo "INFO: Setting public location"
   echo "remote $(wget -q -O - -U curl ipinfo.io/ip) $PORT1 $OVPN_PROTO" > /etc/openvpn/location.conf
 }
 
@@ -121,50 +110,69 @@ function set_public_location {
 # Main setup
 ##############################
 
+function create_zkpath {
+  if [[ $(run_command "find /openvpn") = "" ]]; then
+    run_command "create $ZKPATH '' false false true"
+    run_command "set_acls /$ZKPATH username_password:$OVPN_USERNAME:$OVPN_PASSWORD:cdrwa"
+  fi
+}
+
 function build_configuration {
   # Adding a lock to stop any other instances trying to upload at the same time
-  run_command "create $ZKPATH/upload_marker ''"
-  ovpn_genconfig -u udp://$CA_CN
-  rm -rf $CONFIG_LOCATION/pki
+  echo "INFO: Creating lock file"
+  run_command "create $ZKPATH/upload_marker ''" > /dev/null 2>&1
+  echo "INFO: Resetting container"
+  reset_container
+  echo "INFO: Building configuration"
+  ovpn_genconfig -u udp://$CA_CN > /dev/null 2>&1
+  echo "INFO: Building PKI"
   (echo $CA_CN) | PATH=/dcos/bin:$PATH ovpn_initpki nopass
+  touch /etc/openvpn/complete
 }
 
 function setup {
+  # Introduce a random delay between 1-21 seconds in case of multiple instances starting at the same time
+  sleep $[ ( $RANDOM % 20 )  + 1 ]s
 
-  # Fix a bug in zk-shell copy that's pending a pull request
-  sed -i 's/return PathValue("".os.path.join(fph.readlines()))/return PathValue("".join(os.path.join(fph.readlines())))/g' /usr/lib/python2.7/site-packages/zk_shell-1.1.3-py2.7.egg/zk_shell/copy.py
-  
-  # Replace the shipped easyrsa with our easyrsa to remove the revoke confirmation
-  sed -i 's/easyrsa/\/dcos\/bin\/easyrsa/g' /usr/local/bin/ovpn_revokeclient
-
-  if [ $zookeeper_path = 1 ] && [ $container_files = 0 ]; then
-    echo "Files found in Zookeeper - copying to container"
-    reset_container
-    download_files
-  else
-    echo "Files not found in Zookeeper"
-    if [[ $(run_command "ifind /openvpn/upload_marker") = "" ]]; then
-      echo "Initialising OpenVPN config, pki and uploading to Zookeeper"
-      reset
+  if [[ $(run_command "find /openvpn/ complete") = "" ]]; then
+    echo "INFO: I didn't find a marker signifying a full dataset on Zookeeper"
+    if [[ $(run_command "find /openvpn/ upload_marker") = "" ]]; then
+      echo "INFO: I didn't find a lock"
+      echo "INFO: Creating the zkpath if it doesn't already exist"
+      create_zkpath
       build_configuration
+      echo "INFO: Uploading files to Zookeeper"
       upload_files
+      set_public_location
     else
-      echo "Upload marker found, another instance may be initialising, sleeping for 2m then will attempt download"
-      sleep 2000
+      echo "INFO: Lock found, will random sleep then try again"
       setup
     fi
-  fi 
-    set_public_location
+  else
+    if [[ $(run_command "find /openvpn/ upload_marker") = "" ]]; then
+      reset_container
+      echo "INFO: Files found in Zookeeper, no lock found, downloading to container"
+      download_files
+      set_public_location
+    else
+      echo "INFO: Lock found, will random sleep then try again"
+      setup
+    fi
+  fi
 }
 
 function run_server {
   source /dcos/bin/envs.sh
-  check_status
+  fix_scripts
   setup
+  echo "INFO: Starting crond"
   crond
-  echo "*/2 * * * * /dcos/bin/run.sh synchronise" >> /etc/crontabs/root
+  echo "INFO: Adding cron job for synchronisation"
+  echo "*/2 * * * * /dcos/bin/run.sh synchronise >> /mnt/mesos/sandbox/stdout 2>&1" >> /etc/crontabs/root
+  echo "INFO: Starting OpenVPN daemon"
   ovpn_run --daemon
-  /usr/bin/python -m dcos_openvpn.main
+  echo "INFO: Starting Python REST interface"
+  /usr/bin/python -m dcos_openvpn.main 
 }
 
 function reset {
@@ -182,7 +190,6 @@ case "$@" in
   setup)               setup  ;;
   download_files)      download_files ;;
   upload_files)        upload_files ;;
-  check_status)        check_status ;;
   build_configuration) build_configuration ;;
   set_public_location) set_public_location ;;
   get_location)        get_location ;;
